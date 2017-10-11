2e3934e5477d0a46c4177e788d26aea3976864d3
joifogaca git config --global user.name joifogaca <joi.fogaca@gmail.com>
2017-10-11 11:45:57 -0300

    Criação do Controller Medicos
    Criação das actions e Views Adcionar e Index.

---
+++ CadeOMedico/Controllers/MedicosController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using CadeOMedico.Models;
+
+namespace CadeOMedico.Controllers
+{
+    public class MedicosController : Controller
+    {
+        //
+        // GET: /Medicos/
+
+        private CadeOMedicoBDEntities1 db = new CadeOMedicoBDEntities1();
+
+        public ActionResult Index()
+        {
+            var medicos = db.Medicos.Include(m => m.Cidades)
+                .Include(m => m.Especialidades).ToList();
+            return View(medicos);
+        }
+
+        public ActionResult Adicionar()
+        {
+
+            ViewBag.IDCidade = new SelectList(db.Cidades, "IDCidade", "Nome");
+            ViewBag.IDEspeciade = new SelectList(db.Especialidades, "IDEspecialidade", "Nome");
+            return View();
+        }
+
+    }
+}
