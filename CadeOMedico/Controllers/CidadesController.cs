d9bb736430a056ffdf7beb84547752dcf8302a42
joifogaca git config --global user.name joifogaca <joi.fogaca@gmail.com>
2017-10-19 09:36:34 -0200

    Adcionado o Controler e Views de Cidade e Especialidade
    Adcionada a View de Editar e criação, e Index

---
+++ CadeOMedico/Controllers/CidadesController.cs
@@ -0,0 +1,81 @@
+﻿using CadeOMedico.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CadeOMedico.Controllers
+{
+    public class CidadesController : Controller
+    {
+        private CadeOMedicoBDEntities1 db = new CadeOMedicoBDEntities1();
+        //
+        // GET: /Cidades/
+
+        public ActionResult Index()
+        {
+            var cidades = db.Cidades.ToList();
+            return View(cidades);
+        }
+
+        public ActionResult Adicionar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Adicionar(Cidades cidade)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Cidades.Add(cidade);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return ViewBag(cidade);
+        }
+
+        public ActionResult Edit(long id)
+        {
+            Cidades cidade = db.Cidades.Find(id);
+
+            return View(cidade);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Cidades cidade)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cidade).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+
+            return View(cidade);
+        }
+
+        [HttpPost]
+        public string Excluir(long id)
+        {
+            try
+            {
+               Cidades cidade = db.Cidades.Find(id);
+                db.Cidades.Remove(cidade);
+                db.SaveChanges();
+
+                return Boolean.TrueString;
+
+            }
+            catch
+            {
+                return Boolean.FalseString;
+            }
+        }
+
+    }
+}
---
+++ CadeOMedico/Controllers/EspecialidadesController.cs
@@ -0,0 +1,80 @@
+﻿using CadeOMedico.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CadeOMedico.Controllers
+{
+    public class EspecialidadesController : Controller
+    {
+        private CadeOMedicoBDEntities1 db = new CadeOMedicoBDEntities1();
+        //
+        // GET: /Especialidades/
+
+        public ActionResult Index()
+        {
+            var especialidades = db.Especialidades.ToList();
+            return View(especialidades);
+        }
+
+        public ActionResult Adicionar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Adicionar(Especialidades especialidade)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Especialidades.Add(especialidade);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return ViewBag(especialidade);
+        }
+
+        public ActionResult Edit(long id)
+        {
+            Especialidades especialidade = db.Especialidades.Find(id);
+
+            return View(especialidade);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Especialidades especialidade)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(especialidade).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+
+            return ViewBag(especialidade);
+        }
+
+        [HttpPost]
+        public string Excluir(long id)
+        {
+            try
+            {
+                Especialidades especialidade = db.Especialidades.Find(id);
+                db.Especialidades.Remove(especialidade);
+                db.SaveChanges();
+
+                return Boolean.TrueString;
+
+            }
+            catch
+            {
+                return Boolean.FalseString;
+            }
+        }
+    }
+}
